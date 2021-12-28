6243d4f397be3f9a63fa37186a1d1483adbafbdb
KarolinaTrn168 <karolinatrn@gmail.com>
2021-12-28 10:40:55 +0100

    redisDB

--- RedisDB/fill_redis.py
+++ RedisDB/fill_redis.py
@@ -1,8 +1,9 @@
 import redis
 import connection_redis
-from LearningMode import crawling_endpoints
+import LearningMode
+# from LearningMode import crawling_endpoints
 
-print("baseURL: ", crawling_endpoints.get_baseURL())
+print("baseURL: ", LearningMode.crawling_endpoints.get_baseURL())
 uncleanedEndpoints = open ('LearningMode/working_endpoints.txt', 'r')
 # for x in uncleanedEndpoints:
 #     print x
