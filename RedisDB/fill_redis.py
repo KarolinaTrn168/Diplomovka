016ae8ddde87b35ee25a0eb80a2a5ba93a8dc31d
KarolinaTrn168 <karolinatrn@gmail.com>
2021-12-28 18:15:49 +0100

    some redis changes

--- RedisDB/fill_redis.py
+++ RedisDB/fill_redis.py
@@ -1,16 +1,15 @@
-import redis
 import connection_redis
 from LearningMode.crawling_endpoints import get_baseURL
 
 baseURL = get_baseURL()
-print(baseURL)
 r = connection_redis.connection_redis()
 uncleanedEndpoints = open ('LearningMode/working_endpoints.txt', 'r')
-r.rpush(baseURL, uncleanedEndpoints.readline())
-for x in uncleanedEndpoints:
-    r.rpush(baseURL, x)
+if(r.exists(baseURL)):
+    print("EXISTS!")
+else:
+    r.rpush(baseURL, uncleanedEndpoints.readline())
+    for x in uncleanedEndpoints:
+        r.rpush(baseURL, x)
 
 # print(r.lrange(baseURL, 0, -1))
 
-if(r.exists(baseURL)):
-    print("EXISTS!")
