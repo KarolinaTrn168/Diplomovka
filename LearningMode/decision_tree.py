ba348cee0d7f5032d00caccd0785a6a4e18152e3
KarolinaTrn168 <karolinatrn@gmail.com>
2022-01-03 17:34:46 +0100

    decision tree

--- LearningMode/decision_tree.py
+++ LearningMode/decision_tree.py
@@ -2,7 +2,21 @@ import json
 import string
 from SQLdb.connection_sql import connection_sql
 
-decision = 0
+lower = set(string.ascii_lowercase)
+upper = set(string.ascii_uppercase)
+digits = set(string.digits)
+
+def lower_numbers(value):
+    value = set(value)
+    invalid = value.difference(lower, digits)
+    valid = value.intersection(lower) and value.intersection(digits)
+    return bool(valid and not invalid)
+
+def upper_numbers(value):
+    value = set(value)
+    invalid = value.difference(upper, digits)
+    valid = value.intersection(upper) and value.intersection(digits)
+    return bool(valid and not invalid)
 
 def decision_tree():
     with open('/home/karo/Desktop/Diplomka/Diplomovka/configurations.json', encoding='utf8') as config_file:
@@ -13,7 +27,6 @@ def decision_tree():
     tbl = 'test_table_1'    # will be the name of basic URL
     column_names_list = []; values_list = []
     i = 0; j = 0
-    global decision
 
     mycursor.execute("USE %s" % db)
     # get the name (and count from list) of columns in table
@@ -28,64 +41,53 @@ def decision_tree():
         mycursor.execute("SELECT %s FROM %s" % (column_names_list[j], tbl))
         values = mycursor.fetchall()
         while i < len(values):
-            values_list.append(values[i][-1])
+            if values[i][-1] != None:
+                values_list.append(values[i][-1])
             i = i+1
         # decisions
         # check if every value is the same
         print("values: ", values_list)
-        result = all(element == values_list[0] for element in values_list)    
+        check = 0
+        result = all(element == values_list[0] for element in values_list)
+        result1 = any(element.isalpha() and element.isnumeric() for element in values_list) 
         if result:
-            print("All elements are equal.")
-            # add to scheme
-        else:        
-            print("Not all elements are equal.")
-            condition(string.printable, values_list, 'printable')
-            if decision == 1:
-                condition(string.ascii_letters, values_list, 'lower and upper case')
-                if decision == 1:
-                    condition(string.ascii_lowercase, values_list, 'lower case')
-                    if decision == 1:
+            print("All elements are equal.")  # add to scheme
+        elif result1:        
+            print("Elements contain upper, lower and numbers.") # add to scheme
+        else:
+            result = all(element.isalpha() for element in values_list)
+            if result:
+                result = all(element.isalpha() and element.islower() for element in values_list)
+                if result:
+                    print("All elements are lower case.")
+                else:
+                    result = all(element.isalpha() and element.isupper() for element in values_list)
+                    if result:
+                        print("All elements are upper case.")
+                    else:
+                        print("All elements are letters.")
+            else:
+                result = any(element.isalpha() and element.isupper() for element in values_list) 
+                if not result:
+                    result = all(element.isnumeric() for element in values_list)
+                    if result:
+                        print("All elements are numbers.")
+                    else:
+                        print("Elements contain lower and numbers.")
+                else:
+                    result = any(element.isalpha() and element.islower() for element in values_list) 
+                    if not result:
+                        print("Elements contain upper and numbers.")
+                    else: 
                         result = all(element.isnumeric() for element in values_list)
                         if result:
-                            print("All elements match condition ")
-                            decision = 0
-                            # add to scheme
+                            print("All elements are numbers.")
                         else:
-                            print("Not all elements match condition ")
-                            decision = 1
-       
+                            print("Not all elements are numbers.")       
 
         j = j+1
         i = 0
         values_list.clear()
-
-def condition(option, values_list, msg):
-    global decision
-    result = all(option in element for element in values_list)
-    if result:
-        print("All elements match condition ", msg)
-        decision = 0
-        # add to scheme
-    else:
-        print("Not all elements match condition ", msg)
-        decision = 1
        
 
 decision_tree()
-
-# go to db
-# for each column make:
-# check if parameter already is in schema
-# if already is continue with next parameter
-# for each value in column make:
-# check if contains all characters
-# chcek if contains everything except special characters
-# check if contains numbers
-# check if contains lower and upper case
-# --> if yes print it to scheme and continue with next parameter
-# --> if no go a level under
-
-#advanced: 
-# make a list and make it unique and count
-# if the values are mostly different, than set only the format into the scheme 
-# if there are only 5 different values repeating themselves, probably those are the only options 
