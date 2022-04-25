13e82c71dcce7f25413095ac8d1810804fb2622f
KarolinaTrn168 <karolinatrn@gmail.com>
2022-04-25 16:52:47 +0200

    changes DB

--- LearningMode/convert_params.py
+++ LearningMode/convert_params.py
@@ -1,6 +1,31 @@
 import json
 import string
 from SQLdb.connection_sql import connection_sql
+################################################################################################################################
+
+# The values will be converted into number format, to make it easier to work with them for the machine learning algorithms
+# Numbers will be either the existing ones or zeros (if the announcment is false)
+# Format:
+# 1 = lower case letter
+# 2 = upper case letter
+# 3 = number
+# 4 = interpunction
+# 5 = other special characters
+
+# Counter: (numbers have to be seperated with a ., within the positions with a , if multiple)
+# num = amount of numbers in the value
+# upper = amount of upper case letters in the value 
+# inter = amount of interpunction characters
+# special = amount of special characters
+# pos_num = position where the numbers are 
+# pos_upper = position where the upper case letters are 
+# inter = position where the interpunctions are (for sentences for example)
+
+# Length:
+# len = amount of characters in the value
+
+#################################################################################################################################
+
 
 # load the DB and initialize mydb and mycursor
 with open('/home/karo/Desktop/Diplomka/Diplomovka/configurations.json', encoding='utf8') as config_file:
@@ -10,21 +35,31 @@ mydb, mycursor = connection_sql()
 db = Config['sql']['db_sql']
 
 # select the values from the table, that need to be converted and save them into an array (every string, where format is empty)
-mycursor.execute("SELECT Val FROM Val WHERE Format = "" ")
+mycursor.execute("SELECT Val FROM Val WHERE Format=''")
 myresult = []
 for x in mycursor.fetchall():
     myresult.extend(x)      # pole jednotlivych hodnot
 print(myresult) 
+print(len(myresult))
 
 # from the array convert every string
+format = ""
+counter = ""
+length = 0
+i = 0
+while len(myresult) >= i:
+    actual_parameter = myresult[i]
+
+    i = i+1
+
 # save the result into the database (save it there, where value = converted value)
 
 
 # make number string of format
-format = ""
-format = "".join((format, "1"))
-format = "".join((format, "2"))
-format = "".join((format, "3"))
-format = "".join((format, "4"))
+# format = ""
+# format = "".join((format, "1"))
+# format = "".join((format, "2"))
+# format = "".join((format, "3"))
+# format = "".join((format, "4"))
 
-print(format)
+# print(format)
--- SQLdb/fill_db.py
+++ SQLdb/fill_db.py
@@ -22,123 +22,123 @@ mycursor.execute("USE %s" % db)
 # mycursor.execute("INSERT INTO Parameters (Parameter, URL_id) VALUES ('name', 7), ('last_name', 7), ('age', 7), ('role', 7), ('sex', 7), ('birth_month', 4), ('birth_year', 4), ('valid', 2), ('page_number', 7), ('color', 8), ('password', 8), ('comment', 12), ('extra', 1);")
 
 # fill Values table
-# mycursor.execute("CREATE TABLE IF NOT EXISTS %s (id_val int NOT NULL AUTO_INCREMENT, Val VARCHAR(255), Format VARCHAR(255), Counter VARCHAR(255), Length VARCHAR(255), Param_id int, PRIMARY KEY (id_val), FOREIGN KEY(Param_id) REFERENCES Parameters(id_param))" % tbl_val)
-# mycursor.execute("INSERT INTO Val (Val, Format, Counter, Length, Param_id) VALUES \
-#     ('John', NULL, NULL, NULL, 1), \
-#     ('Mark', NULL, NULL, NULL, 1), \
-#     ('Lucy', NULL, NULL, NULL, 1), \
-#     ('Angel', NULL, NULL, NULL, 1), \
-#     ('Victor', NULL, NULL, NULL, 1), \
-#     ('Wick', NULL, NULL, NULL, 2), \
-#     ('Doe', NULL, NULL, NULL, 2), \
-#     ('McDonald', NULL, NULL, NULL, 2), \
-#     ('Scott', NULL, NULL, NULL, 2), \
-#     ('Long', NULL, NULL, NULL, 2), \
-#     ('32', NULL, NULL, NULL, 3), \
-#     ('24', NULL, NULL, NULL, 3), \
-#     ('8', NULL, NULL, NULL, 3), \
-#     ('97', NULL, NULL, NULL, 3), \
-#     ('9', NULL, NULL, NULL, 3), \
-#     ('admin', NULL, NULL, NULL, 4), \
-#     ('user', NULL, NULL, NULL, 4), \
-#     ('user', NULL, NULL, NULL, 4), \
-#     ('admin', NULL, NULL, NULL, 4), \
-#     ('reader', NULL, NULL, NULL, 4), \
-#     ('male', NULL, NULL, NULL, 5), \
-#     ('female', NULL, NULL, NULL, 5), \
-#     ('male', NULL, NULL, NULL, 5), \
-#     ('female', NULL, NULL, NULL, 5), \
-#     ('female', NULL, NULL, NULL, 5), \
-#     ('August', NULL, NULL, NULL, 6), \
-#     ('July', NULL, NULL, NULL, 6), \
-#     ('September', NULL, NULL, NULL, 6), \
-#     ('October', NULL, NULL, NULL, 6), \
-#     ('March', NULL, NULL, NULL, 6), \
-#     ('1999', NULL, NULL, NULL, 7), \
-#     ('2003', NULL, NULL, NULL, 7), \
-#     ('1945', NULL, NULL, NULL, 7), \
-#     ('2001', NULL, NULL, NULL, 7), \
-#     ('2016', NULL, NULL, NULL, 7), \
-#     ('yes', NULL, NULL, NULL, 8), \
-#     ('yes', NULL, NULL, NULL, 8), \
-#     ('yes', NULL, NULL, NULL, 8), \
-#     ('no', NULL, NULL, NULL, 8), \
-#     ('no', NULL, NULL, NULL, 8), \
-#     ('21', NULL, NULL, NULL, 9), \
-#     ('3', NULL, NULL, NULL, 9), \
-#     ('123', NULL, NULL, NULL, 9), \
-#     ('3456', NULL, NULL, NULL, 9), \
-#     ('263', NULL, NULL, NULL, 9), \
-#     ('red', NULL, NULL, NULL, 10), \
-#     ('green', NULL, NULL, NULL, 10), \
-#     ('purple', NULL, NULL, NULL, 10), \
-#     ('blue', NULL, NULL, NULL, 10), \
-#     ('red', NULL, NULL, NULL, 10), \
-#     ('pass?123.', NULL, NULL, NULL, 11), \
-#     ('heslO_22?!', NULL, NULL, NULL, 11), \
-#     ('HeSL0(ine)/', NULL, NULL, NULL, 11), \
-#     ('.He$I_0*,*', NULL, NULL, NULL, 11), \
-#     ('P%S$W0&#3', NULL, NULL, NULL, 11), \
-#     ('This is a nice comment.', NULL, NULL, NULL, 12), \
-#     ('This too.', NULL, NULL, NULL, 12), \
-#     ('Not so good.', NULL, NULL, NULL, 12), \
-#     ('With question?', NULL, NULL, NULL, 12), \
-#     ('Great!', NULL, NULL, NULL, 12), \
-#     ('Extra12@”', NULL, NULL, NULL, 13), \
-#     ('!@”#$%”', NULL, NULL, NULL, 13), \
-#     ('? ? @# %^ 8', NULL, NULL, NULL, 13), \
-#     ('C#45v\45-\2', NULL, NULL, NULL, 13), \
-#     ('k0v32.,\', NULL, NULL, NULL, 13), \
-#     ('$', NULL, NULL, NULL, 13), \
-#     ('November', NULL, NULL, NULL, 6), \
-#     ('December', NULL, NULL, NULL, 6), \
-#     ('August', NULL, NULL, NULL, 6), \
-#     ('April', NULL, NULL, NULL, 6), \
-#     ('January', NULL, NULL, NULL, 6), \
-#     ('March', NULL, NULL, NULL, 6), \
-#     ('April', NULL, NULL, NULL, 6), \
-#     ('male', NULL, NULL, NULL, 5), \
-#     ('male', NULL, NULL, NULL, 5), \
-#     ('female', NULL, NULL, NULL, 5), \
-#     ('male', NULL, NULL, NULL, 5), \
-#     ('female', NULL, NULL, NULL, 5), \
-#     ('pink', NULL, NULL, NULL, 10), \
-#     ('orange', NULL, NULL, NULL, 10), \
-#     ('green', NULL, NULL, NULL, 10), \
-#     ('yellow', NULL, NULL, NULL, 10), \
-#     ('red', NULL, NULL, NULL, 10), \
-#     ('blue', NULL, NULL, NULL, 10), \
-#     ('violet', NULL, NULL, NULL, 10), \
-#     ('brown', NULL, NULL, NULL, 10), \
-#     ('black', NULL, NULL, NULL, 10), \
-#     ('white', NULL, NULL, NULL, 10), \
-#     ('admin', NULL, NULL, NULL, 4), \
-#     ('admin', NULL, NULL, NULL, 4), \
-#     ('user', NULL, NULL, NULL, 4), \
-#     ('reader', NULL, NULL, NULL, 4), \
-#     ('reader', NULL, NULL, NULL, 4), \
-#     ('user', NULL, NULL, NULL, 4), \
-#     ('admin', NULL, NULL, NULL, 4), \
-#     ('user', NULL, NULL, NULL, 4), \
-#     ('kdjsfh8340/', NULL, NULL, NULL, 11), \
-#     ('f3oi3]4', NULL, NULL, NULL, 11), \
-#     ('c,[34[3', NULL, NULL, NULL, 11), \
-#     ('32p4ddlDKS', NULL, NULL, NULL, 11), \
-#     ('W$ROp4.', NULL, NULL, NULL, 11), \
-#     ('pj4DL#lke', NULL, NULL, NULL, 11), \
-#     ('lknp34SK', NULL, NULL, NULL, 11), \
-#     ('|@#$”|”>$', NULL, NULL, NULL, 13), \
-#     ('!#|#?$|@$}F#', NULL, NULL, NULL, 13), \
-#     ('2p3@#|$', NULL, NULL, NULL, 13), \
-#     ('1Z#@{z/\<', NULL, NULL, NULL, 13), \
-#     ('P#”>”#{.z  “S', NULL, NULL, NULL, 13), \
-#     ('#$}sp2SF$L@”', NULL, NULL, NULL, 13), \
-#     ('multiple word', NULL, NULL, NULL, 12), \
-#     ('doesn’t have to be a sentence ', NULL, NULL, NULL, 12), \
-#     ('I don’t believe', NULL, NULL, NULL, 12), \
-#     ('Every. Word. Is a sentence.', NULL, NULL, NULL, 12), \
-#     ('Questioning is great?', NULL, NULL, NULL, 12);")
-# # convert values and add missing columns (that are set NULL)
+mycursor.execute("CREATE TABLE IF NOT EXISTS %s (id_val int NOT NULL AUTO_INCREMENT, Val VARCHAR(255), Format VARCHAR(255), Counter VARCHAR(255), Length VARCHAR(255), Param_id int, PRIMARY KEY (id_val), FOREIGN KEY(Param_id) REFERENCES Parameters(id_param))" % tbl_val)
+mycursor.execute("INSERT INTO Val (Val, Format, Counter, Length, Param_id) VALUES \
+    ('John', '', '', '', 1), \
+    ('Mark', '', '', '', 1), \
+    ('Lucy', '', '', '', 1), \
+    ('Angel', '', '', '', 1), \
+    ('Victor', '', '', '', 1), \
+    ('Wick', '', '', '', 2), \
+    ('Doe', '', '', '', 2), \
+    ('McDonald', '', '', '', 2), \
+    ('Scott', '', '', '', 2), \
+    ('Long', '', '', '', 2), \
+    ('32', '', '', '', 3), \
+    ('24', '', '', '', 3), \
+    ('8', '', '', '', 3), \
+    ('97', '', '', '', 3), \
+    ('9', '', '', '', 3), \
+    ('admin', '', '', '', 4), \
+    ('user', '', '', '', 4), \
+    ('user', '', '', '', 4), \
+    ('admin', '', '', '', 4), \
+    ('reader', '', '', '', 4), \
+    ('male', '', '', '', 5), \
+    ('female', '', '', '', 5), \
+    ('male', '', '', '', 5), \
+    ('female', '', '', '', 5), \
+    ('female', '', '', '', 5), \
+    ('August', '', '', '', 6), \
+    ('July', '', '', '', 6), \
+    ('September', '', '', '', 6), \
+    ('October', '', '', '', 6), \
+    ('March', '', '', '', 6), \
+    ('1999', '', '', '', 7), \
+    ('2003', '', '', '', 7), \
+    ('1945', '', '', '', 7), \
+    ('2001', '', '', '', 7), \
+    ('2016', '', '', '', 7), \
+    ('yes', '', '', '', 8), \
+    ('yes', '', '', '', 8), \
+    ('yes', '', '', '', 8), \
+    ('no', '', '', '', 8), \
+    ('no', '', '', '', 8), \
+    ('21', '', '', '', 9), \
+    ('3', '', '', '', 9), \
+    ('123', '', '', '', 9), \
+    ('3456', '', '', '', 9), \
+    ('263', '', '', '', 9), \
+    ('red', '', '', '', 10), \
+    ('green', '', '', '', 10), \
+    ('purple', '', '', '', 10), \
+    ('blue', '', '', '', 10), \
+    ('red', '', '', '', 10), \
+    ('pass?123.', '', '', '', 11), \
+    ('heslO_22?!', '', '', '', 11), \
+    ('HeSL0(ine)/', '', '', '', 11), \
+    ('.He$I_0*,*', '', '', '', 11), \
+    ('P%S$W0&#3', '', '', '', 11), \
+    ('This is a nice comment.', '', '', '', 12), \
+    ('This too.', '', '', '', 12), \
+    ('Not so good.', '', '', '', 12), \
+    ('With question?', '', '', '', 12), \
+    ('Great!', '', '', '', 12), \
+    ('Extra12@”', '', '', '', 13), \
+    ('!@”#$%”', '', '', '', 13), \
+    ('? ? @# %^ 8', '', '', '', 13), \
+    ('C#45v\45-\2', '', '', '', 13), \
+    ('k0v32.,\', '', '', '', 13), \
+    ('$', '', '', '', 13), \
+    ('November', '', '', '', 6), \
+    ('December', '', '', '', 6), \
+    ('August', '', '', '', 6), \
+    ('April', '', '', '', 6), \
+    ('January', '', '', '', 6), \
+    ('March', '', '', '', 6), \
+    ('April', '', '', '', 6), \
+    ('male', '', '', '', 5), \
+    ('male', '', '', '', 5), \
+    ('female', '', '', '', 5), \
+    ('male', '', '', '', 5), \
+    ('female', '', '', '', 5), \
+    ('pink', '', '', '', 10), \
+    ('orange', '', '', '', 10), \
+    ('green', '', '', '', 10), \
+    ('yellow', '', '', '', 10), \
+    ('red', '', '', '', 10), \
+    ('blue', '', '', '', 10), \
+    ('violet', '', '', '', 10), \
+    ('brown', '', '', '', 10), \
+    ('black', '', '', '', 10), \
+    ('white', '', '', '', 10), \
+    ('admin', '', '', '', 4), \
+    ('admin', '', '', '', 4), \
+    ('user', '', '', '', 4), \
+    ('reader', '', '', '', 4), \
+    ('reader', '', '', '', 4), \
+    ('user', '', '', '', 4), \
+    ('admin', '', '', '', 4), \
+    ('user', '', '', '', 4), \
+    ('kdjsfh8340/', '', '', '', 11), \
+    ('f3oi3]4', '', '', '', 11), \
+    ('c,[34[3', '', '', '', 11), \
+    ('32p4ddlDKS', '', '', '', 11), \
+    ('W$ROp4.', '', '', '', 11), \
+    ('pj4DL#lke', '', '', '', 11), \
+    ('lknp34SK', '', '', '', 11), \
+    ('|@#$”|”>$', '', '', '', 13), \
+    ('!#|#?$|@$}F#', '', '', '', 13), \
+    ('2p3@#|$', '', '', '', 13), \
+    ('1Z#@{z/\<', '', '', '', 13), \
+    ('P#”>”#{.z  “S', '', '', '', 13), \
+    ('#$}sp2SF$L@”', '', '', '', 13), \
+    ('multiple word', '', '', '', 12), \
+    ('doesn’t have to be a sentence ', '', '', '', 12), \
+    ('I don’t believe', '', '', '', 12), \
+    ('Every. Word. Is a sentence.', '', '', '', 12), \
+    ('Questioning is great?', '', '', '', 12);")
+# # convert values and add missing columns (that are set '')
 
 mydb.commit()
 print(mycursor.rowcount, "was inserted.")
